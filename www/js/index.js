de20b12246a188342f15d2886de2bcb5efb85918
mubarak <mubarak@qburst.com>
2015-12-04 20:05:25 +0530

    Added bootstrap

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,9 @@
  * under the License.
  */
 var app = {
+    
+    baseUrl: "http://localhost:3002",
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -37,33 +40,15 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        var onSuccess = function(position) {
-            alert('Latitude: '          + position.coords.latitude          + '\n' +
-                'Longitude: '         + position.coords.longitude         + '\n' +
-                'Altitude: '          + position.coords.altitude          + '\n' +
-                'Accuracy: '          + position.coords.accuracy          + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                'Heading: '           + position.coords.heading           + '\n' +
-                'Speed: '             + position.coords.speed             + '\n' +
-                'Timestamp: '         + position.timestamp                + '\n');
-        };
-
-        // onError Callback receives a PositionError object
-        //
-        function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-        }
-        var options = { enableHighAccuracy: true };
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+        
+    },
+    
+    hideIndicator: function() {
+        $("#loading").hide();
+    },
+    
+    showIndicator: function() {
+        $("#loading").show();
     }
 };
 
